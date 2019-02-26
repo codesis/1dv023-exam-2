b48a224da0f12e9d7c78dd3c41a03b8b4aee41c5
emma <hassling.emma@gmail.com>
2019-02-26 11:18:55 +0100

    Adding basic in app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const express = require('express')
+const path = require('path')
+const app = express()
+const hbs = require('express-hbs')
+const logger = require('morgan')
+
+// view engine setup
+app.engine('hbs', hbs.express4({
+  defaultLayout: path.join(__dirname, 'views', 'layouts', 'default')
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+// additional middleware
+app.use(logger('dev'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+// routes
+app.use('/', require('./routes/homeRouter'))
+// catch 404
+app.use((req, res, next) => {
+  res.status(404)
+  res.sendFile(path.join(__dirname, 'public', '404.html'))
+})
+// error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.send(err.message || 'Internal Server Error')
+})
